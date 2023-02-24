2bd48691fcda9e8adf977a2c5994561f2bd817a7
Vasily.Mordus <iphoje@gmail.com>
2023-02-24 12:30:02 +0100

    Project created

---
+++ bank-edu-app/bank-edu-appUITests/bank_edu_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bank_edu_appUITestsLaunchTests.swift
+//  bank-edu-appUITests
+//
+//  Created by Shirayo on 24.02.2023.
+//
+
+import XCTest
+
+final class bank_edu_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
